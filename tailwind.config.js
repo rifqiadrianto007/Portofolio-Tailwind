353d4c521a6b9720b14e0152c7a793db4caf6a87
rifqiadrianto007 <rifqiadrianto77@gmail.com>
2024-10-05 22:16:31 +0700

    content update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         primary : '#3b82f6',
         secondary : '#64748b',
         dark : '#0f172a',
+        animation: {
+          'spin-slow': 'spin 3s linear infinite',
+          'goyang': 'goyang 1s ease-in-out infinite'
+        },
+        keyframes: {
+          goyang: {
+            '0%, 100%' : {transform: 'rotate (-3deg)'},
+            '50%': {transform: 'rotate(3deg)'},
+          },
+        },
       },
       screens : {
         '2xl' : '1320px',
